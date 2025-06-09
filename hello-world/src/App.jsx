9176c8de1d4a25a5f35e0027719275514546afe6
EwgenyKuzmenko <46458547+EwgenyKuzmenko@users.noreply.github.com>
2025-06-09 03:01:25 +0300

    Improve home page

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Products from './Products.jsx'
-
-function Home() {
-  return (
-    <section className="hero">
-      <h1>КомКом</h1>
-      <p>Лучшие телевизоры для вашего дома</p>
-      <Link to="/tvs" className="cta-button">К товарам</Link>
-    </section>
-  )
-}
+import Phones from './Phones.jsx'
+import Home from './Home.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/tvs" element={<Products />} />
+      <Route path="/phones" element={<Phones />} />
     </Routes>
   )
 }
